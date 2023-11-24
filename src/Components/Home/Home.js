152a9381fd42bb67244b4762b9f6513b48bb8d66
shekar <shekarraju47@outlook.com>
2023-11-24 23:34:41 +0530

    adding apis

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,23 +1,94 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const apiConstrant = {
+  initial: "INITIAL",
+  loading: "LOADING",
+  success: "SUCCSESS",
+  failure: "FAILURE",
+};
 
 function Home() {
+  const [data, setData] = useState([]);
+  const [apiStatus, setStatus] = useState({
+    api: apiConstrant.initial,
+    error: false,
+    errorMsg: null,
+  });
+
   useEffect(() => {
     const getData = async () => {
-      const url = "http://localhost:5000/";
+      setStatus((prev) => ({
+        ...prev,
+        api: apiConstrant.loading,
+      }));
+      const url = "http://localhost:500/";
       const options = {
         method: "get",
       };
-
-      const res = await fetch(url, options);
-      const data = await res.json();
-      console.log(data);
+      try {
+        const res = await fetch(url, options);
+        if (res.ok) {
+          const resdata = await res.json();
+          setStatus((prev) => ({
+            ...prev,
+            api: apiConstrant.success,
+          }));
+          setData(resdata);
+          console.log(resdata);
+        } else {
+          setStatus((prev) => ({
+            ...prev,
+            api: apiConstrant.failure,
+            error: true,
+            errorMsg: "Something Wrong",
+          }));
+          console.log("error");
+        }
+      } catch (e) {
+        setStatus((prev) => ({
+          ...prev,
+          api: apiConstrant.failure,
+          error: true,
+          errorMsg: "Error",
+        }));
+      }
     };
     getData();
   }, []);
 
+  console.log(apiStatus);
+
+  const Loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  const Success = () => {
+    return <h1>SuccessFull</h1>;
+  };
+
+  const Failure = () => {
+    return <h1>Failed</h1>;
+  };
+
+  const getResult = () => {
+    const { api } = apiStatus;
+    console.log(api);
+    switch (api) {
+      case apiConstrant.loading:
+        return Loading();
+      case apiConstrant.success:
+        return Success();
+      case apiConstrant.failure:
+        return Failure();
+      default:
+        return null;
+    }
+  };
+
   return (
     <div>
       <h1>Home</h1>
+      {getResult()}
     </div>
   );
 }
