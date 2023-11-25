bfadf31beff46dc3e56a3f9deeb85376f8fde08e
shekar <shekarraju47@outlook.com>
2023-11-25 08:37:54 +0530

    set api

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -21,7 +21,7 @@ function Home() {
         ...prev,
         api: apiConstrant.loading,
       }));
-      const url = "http://localhost:500/";
+      const url = "http://localhost:5000/";
       const options = {
         method: "get",
       };
