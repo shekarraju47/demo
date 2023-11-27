ee6152ac42c65f011f20224f73865706899abdbe
shekar <shekarraju47@outlook.com>
2023-11-27 12:07:06 +0530

    context added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
+import React, { useState, createContext } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Form from "./Components/Userform/Form";
 import Header from "./Components/Header/Header";
 
+export const DarkmodeContext = createContext();
+
+export const isdark =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+let bgColor;
+let color;
+
 function App() {
+  const [mode, setMode] = useState(isdark);
+
+  if (mode) {
+    bgColor = "#1a1a1a";
+    color = "#f5f5f5";
+  } else {
+    bgColor = "#f5f5f5";
+    color = "#1a1a1a";
+  }
   return (
-    <BrowserRouter basename="/demo">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signup/" component={Form} />
-      </Switch>
-    </BrowserRouter>
+    <div style={{ backgroundColor: bgColor, color: color }}>
+      <DarkmodeContext.Provider value={[mode, setMode]}>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup/" component={Form} />
+          </Switch>
+        </BrowserRouter>
+      </DarkmodeContext.Provider>
+    </div>
   );
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,18 @@
 import { Link, withRouter } from "react-router-dom";
+import { useContext } from "react";
+import { DarkmodeContext } from "../../App";
+
 import "./index.css";
 
 function Header() {
+  const [mode, setMode] = useContext(DarkmodeContext);
+  const SelectDark = () => {
+    console.log(mode);
+    setMode((prev) => !prev);
+  };
   return (
     <div className="header-main-cont">
+      <button onClick={SelectDark}>isdark</button>
       <Link to="/">
         <button className="link">Home</button>
       </Link>
